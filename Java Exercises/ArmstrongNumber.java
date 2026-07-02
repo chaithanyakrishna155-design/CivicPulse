2267246a3454fc0254a09b8a6a989fa175708dfd
chaithanyakrishna155-design <chaithanyakrishna155@gmail.com>
2026-07-02 23:06:18 +0530

    Add files via upload

---
+++ Java Exercises/ArmstrongNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int temp = num, sum = 0;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+        System.out.println(num + (sum == num ? " is Armstrong." : " is not Armstrong."));
+    }
+}
